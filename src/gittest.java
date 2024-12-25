3ac64dd526ef1235f557875f50551442782c8669
VoidR01170123 <asj01170123@gmail.com>
2024-12-25 21:48:00 +0900

    git test

---
+++ src/gittest.java
@@ -0,0 +1,10 @@
+public class gittest {
+    public static void main(String[] args) {
+        int sum = add(1,2);
+        System.out.println(sum);
+    }
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
