5375698b911e0e2465da0e625c7d97be542e31e3
VoidR01170123 <asj01170123@gmail.com>
2024-12-28 08:07:13 +0900

    2024_git config --global user.name asj01170123@gmail.com_28

--- src/class1/Ex.java
+++ src/class1/Ex.java
@@ -2,8 +2,8 @@ package class1;
 
 public class Ex {
     public static void main(String[] args) {
-        Student student1;
-        student1 = new Student();
+        Student student1; //Student 클래스 선언( int a; 같은 느낌)
+        student1 = new Student(); //메모리 만들기 ( int a 에 메모리 할당하는 느낌)
         //1. 객체 값 읽기
         System.out.println("이름:" + student1.name);
         //2. 변수에 있는 참조값을 통해 실제 객체에 접근하고, name 멤버 변수에 접근하다.
