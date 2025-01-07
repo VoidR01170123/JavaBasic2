372c477ab1cde9d2961c17c40c510fbb756ca2f8
VoidR01170123 <asj01170123@gmail.com>
2025-01-07 19:29:04 +0900

    2025_01_07

--- src/class1/ex/MovieReview.java
+++ src/class1/ex/MovieReview.java
@@ -4,4 +4,4 @@ public class MovieReview {
     String title;
     String review;
 }
-
+//클래스 안의 요소들을 필드라고 함.
--- src/class1/ex/ProductOrderMain1.java
+++ src/class1/ex/ProductOrderMain1.java
@@ -2,11 +2,10 @@ package class1.ex;
 
 public class ProductOrderMain1 {
     public static void main(String[] args) {
-        ProductOrder[] prodcutorders = new ProductOrder[3];
-        int sum = 0;
+        ProductOrder[] prodcutorders = new ProductOrder[3]; //ProductOrder 클래스를 받을 수 있는 크기 3의 배열 productorders 공간 할당
 
-        ProductOrder productorder1 = new ProductOrder();
-        productorder1.productName = "두부";
+        ProductOrder productorder1 = new ProductOrder(); // 객체 저장 공간 할당
+        productorder1.productName = "두부"; //할당한 공간으로 접근해서 작성
         productorder1.price = 2000;
         productorder1.quantity = 2;
         prodcutorders[0] = productorder1;
@@ -23,7 +22,9 @@ public class ProductOrderMain1 {
         productorder3.quantity = 2;
         prodcutorders[2] = productorder3;
 
-        for (ProductOrder prodcutorder : prodcutorders) {
+        int sum = 0; //총 결제 금액 저장을 위한 변수 선언
+        
+        for (ProductOrder prodcutorder : prodcutorders) { //for-each문. 배열 productorders에 접근해, 그 안의 요소들을 전부 prodcutorder에 넣고 아래 블록을 실행
             System.out.println("상품명: " + prodcutorder.productName + ". 가격: " + prodcutorder.price + ", 수량: " + prodcutorder.quantity);
             sum += prodcutorder.price * prodcutorder.quantity;
         }
