b43091d05d9895fc28dea941563fb3fedccd1050
VoidR01170123 <asj01170123@gmail.com>
2025-01-15 19:31:28 +0900

    2025_01_15

--- src/pack/PackageMain2.java
+++ src/pack/PackageMain2.java
@@ -6,6 +6,6 @@ public class PackageMain2 {
     public static void main(String[] args) {
      Data data = new Data();
      User user = new User();
-     User2 user2 = new User2();
+     User2 user2 = new User2(); //import 사용으로 패키지 명 생략 가능, *로 해당 패키지 안 모두 사용 가능
     }
 }
