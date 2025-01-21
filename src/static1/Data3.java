eaa9e6c9a05e5b3e6763a8b37a2e0b77c5b4a4b0
VoidR01170123 <asj01170123@gmail.com>
2025-01-21 21:06:09 +0900

    2025_01_21

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -2,7 +2,8 @@ package static1;
 
 public class Data3 {
     public String name;
-    public static int count; //static
+    public static int count; //static // name과 count 둘 다 멤버변수(필드)다. 이 중 static이 붙은 것은 클래스 변수(정적변수, static 변수),
+    // 안붙은 것은 인스턴스 변수라고 한다.
 
     public Data3(String name) {
         this.name = name;
