da5a90aa5f7531589873021a2e6b4f6a61265c3e
VoidR01170123 <asj01170123@gmail.com>
2025-01-23 21:30:27 +0900

    2025_01_23

--- src/static2/ex/MathArrayUtils.java
+++ src/static2/ex/MathArrayUtils.java
@@ -2,22 +2,39 @@ package static2.ex;
 
 public class MathArrayUtils {
 
-    private static int sum;
-    private static int count;
+    private MathArrayUtils() {
+        //private 생성자를 만들어 인스턴스 생성을 막는다.
+    }
 
-    public static int sum(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
-            count++;
+    public static int sum(int[] values) {
+        int total = 0;
+        for (int value : values) {
+            total += value;
         }
-        return sum;
+        return total;
     }
 
-    public static int average(int[] array) {
-        int average = 0;
-        average = sum / count;
-
-        return average;
+    public static double average(int[] values) {
+        return (double)sum(values) / values.length;
     }
 
+    public static int min(int[] values) {
+        int minimum1 = values[0];
+        for (int value : values) {
+            if (value < minimum1) {
+                minimum1 = value;
+            }
+        }
+        return minimum1;
+    }
+
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int value : values) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
 }
