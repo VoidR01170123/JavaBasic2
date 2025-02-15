716c9ed7579fb69dfdd01bbfd1f036e9e8e37c91
VoidR01170123 <asj01170123@gmail.com>
2025-02-15 22:48:15 +0900

    2025_02_15

--- src/poly/basic/CastingMain4.java
+++ src/poly/basic/CastingMain4.java
@@ -1,6 +1,8 @@
 package poly.basic;
 
+//다운캐스팅을 자동으로 하지 않는 이유
 public class CastingMain4 {
+
     public static void main(String[] args) {
         Parent parent1 = new Child();
         Child child1 = (Child) parent1;
